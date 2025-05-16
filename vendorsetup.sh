a41e2e40f43365c77a0e420eec0c20ab159d617b
Dhanush /.\./. <106550362+dhanush281@users.noreply.github.com>
2025-05-16 02:12:32 -0700

    sky:Added bcr,kernel,agm,audio-pal,hardware/xiaomi

---
+++ vendorsetup.sh
@@ -0,0 +1,20 @@
+# BCR
+git clone https://github.com/suvojit213/vendor_bcr.git vendor/bcr
+
+# Clone the kernel source
+echo "Cloning kernel repository..."
+git clone https://github.com/dhanush281/android_xiaomi_sky_kernel.git device/xiaomi/sky-kernel
+
+# Clone the hardware repository
+echo "Cloning hardware repository..."
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
+
+# Clone the agm repository
+echo "cloning agm repository..."
+rm -rf hardware/qcom-caf/sm8450/audio/agm && git clone https://github.com/dhanush281/agm.git hardware/qcom-caf/sm8450/audio/agm
+
+echo "done"
+
+rm -rf hardware/qcom-caf/sm8450/audio/pal && git clone https://github.com/lostark13/arpal-lx hardware/qcom-caf/sm8450/audio/pal
+
+echo "done"
