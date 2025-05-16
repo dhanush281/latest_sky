e0cd39285746064d9c1d1d46a0312dd43c427dfd
Dhanush /.\./. <106550362+dhanush281@users.noreply.github.com>
2025-05-16 06:47:24 -0700

    welcome to horizon

--- device.mk
+++ device.mk
@@ -385,7 +385,7 @@ PRODUCT_SHIPPING_API_LEVEL := 31
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH) \
-    hardware/pixelage/interfaces/power-libperfmgr \
+    hardware/lineage/interfaces/power-libperfmgr \
     hardware/google \
     hardware/google/interfaces \
     hardware/google/pixel \
