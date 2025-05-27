d29bdd2644d5526791afa9ad84438193040c7542
Dhanush /.\./. <106550362+dhanush281@users.noreply.github.com>
2025-05-26 22:09:55 -0700

    sky:Updated Kernel Repo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,7 +3,7 @@ git clone https://github.com/suvojit213/vendor_bcr.git vendor/bcr
 
 # Clone the kernel source
 echo "Cloning kernel repository..."
-git clone https://github.com/dhanush281/android_xiaomi_sky_kernel.git device/xiaomi/sky-kernel
+git clone https://github.com/BlissRoms-Devices/android_kernel_xiaomi_sky device/xiaomi/sky-kernel
 
 # Clone the hardware repository
 echo "Cloning hardware repository..."
