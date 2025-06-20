a6a1b9f94660db0d84678b61a733778fe23cc272
Dhanush /.\./. <106550362+dhanush281@users.noreply.github.com>
2025-06-19 18:46:01 -0700

    sky:Arise AxionOs

--- lineage_sky.mk
+++ lineage_sky.mk
@@ -23,6 +23,32 @@ $(call inherit-product, $(DEVICE_PATH)/device.mk)
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/sky/sky-vendor.mk)
 
+# Define rear camera specs (multiple sensors supported)
+AXION_CAMERA_REAR_INFO := 50,2  # Example: 50MP + 2MP
+
+# Define front camera specs
+AXION_CAMERA_FRONT_INFO := 8  # Example: 8MP
+
+# Maintainer name (use "_" for spaces, e.g., "rmp_22" → "rmp 22" in UI)
+AXION_MAINTAINER := Dhanush_x_Ashwani
+
+# Processor name (use "_" for spaces)
+AXION_PROCESSOR := Snapdragon®_4_gen_2
+
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_ENABLE_BLUR := false
+
+AXION_CPU_SMALL_CORES     := 0,1,2,3,4,5
+AXION_CPU_BIG_CORES       := 6,7
+AXION_CPU_UNLIMIT_UI      := 0-7
+AXION_CPU_BG              := 0-3
+AXION_CPU_FG              := 0-6
+AXION_CPU_LIMIT_BG        := 0-2
+AXION_CPU_LIMIT_UI        := 0-5
+
+# Wether to enable debugging for adb logcat purposes
+AXION_DEBUGGING_ENABLED := false
+
 ## Device identifier
 PRODUCT_DEVICE := sky
 PRODUCT_NAME := lineage_sky
