d959f37d6490756a159adfd7ece65aa4a539f252
lostark13 <tushar9915@gmail.com>
2025-06-21 12:11:54 -0700

    sky: vibrator rebrand

--- vibrator/main.cpp
+++ vibrator/main.cpp
@@ -15,29 +15,19 @@
  */
 
 #include "vibrator-impl/Vibrator.h"
-#include "vibrator-impl/VibratorManager.h"
 
 #include <android-base/logging.h>
 #include <android/binder_manager.h>
 #include <android/binder_process.h>
 
 using aidl::android::hardware::vibrator::Vibrator;
-using aidl::android::hardware::vibrator::VibratorManager;
 
 int main() {
     ABinderProcess_setThreadPoolMaxThreadCount(0);
-
-    // make a default vibrator service
-    auto vib = ndk::SharedRefBase::make<Vibrator>();
-    binder_status_t status = AServiceManager_addService(
-            vib->asBinder().get(), Vibrator::makeServiceName("default").c_str());
-    CHECK_EQ(status, STATUS_OK);
-
-    // make the vibrator manager service with a different vibrator
-    auto managedVib = ndk::SharedRefBase::make<Vibrator>();
-    auto vibManager = ndk::SharedRefBase::make<VibratorManager>(std::move(managedVib));
-    status = AServiceManager_addService(vibManager->asBinder().get(),
-                                        VibratorManager::makeServiceName("default").c_str());
+    std::shared_ptr<Vibrator> vib = ndk::SharedRefBase::make<Vibrator>();
+ 
+    const std::string instance = std::string() + Vibrator::descriptor + "/default";
+    binder_status_t status = AServiceManager_addService(vib->asBinder().get(), instance.c_str());
     CHECK_EQ(status, STATUS_OK);
 
     ABinderProcess_joinThreadPool();
